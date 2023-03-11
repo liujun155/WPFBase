349d0de04a6ad40d62e3574d1101dc0007c40093
liujun <354277337@qq.com>
2023-03-12 00:25:26 +0800

    对象帧动画；附加属性

---
+++ WPF依赖属性附加属性/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF依赖属性附加属性
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region 附加属性
+        public static bool GetVisible(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(VisibleProperty);
+        }
+
+        public static void SetVisible(DependencyObject obj, bool value)
+        {
+            obj.SetValue(VisibleProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Visible.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty VisibleProperty =
+            DependencyProperty.RegisterAttached("Visible", typeof(bool), typeof(MainWindow), new PropertyMetadata(true, OnVisibleChanged));
+
+        private static void OnVisibleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            bool visible = (bool)e.NewValue;
+            if (visible)
+                d.SetValue(UIElement.VisibilityProperty, Visibility.Visible);
+            else
+                d.SetValue(UIElement.VisibilityProperty, Visibility.Collapsed);
+        }
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
